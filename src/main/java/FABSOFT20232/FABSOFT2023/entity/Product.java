b18007463bf95d56ba3e4ac4f1514e14f63644e0
José Otávio <101358660+devJoseOtavio@users.noreply.github.com>
2023-09-04 19:17:17 -0300

    adicionando controladores e domain

---
+++ src/main/java/FABSOFT20232/FABSOFT2023/entity/Product.java
@@ -0,0 +1,12 @@
+//package FABSOFT20232.FABSOFT2023.entity;
+//
+//import java.math.BigDecimal;
+//
+//public class Product {
+//
+//    private String name;
+//
+//    private BigDecimal value;
+//
+//    ProductType ProductType;
+//}
---
+++ src/main/java/FABSOFT20232/FABSOFT2023/enum/ProductType.java
@@ -0,0 +1,8 @@
+//package FABSOFT20232.FABSOFT2023.enum;
+//
+//public enum ProductType {
+//
+//    String whisky;
+//
+//    String beer;
+//}
